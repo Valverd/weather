ab197087dad848e555d9d335aa56b9b3447e1f02
Renan <renanvgmartins@hotmail.com>
2024-12-02 20:58:40 -0300

    Corrigir problema de type city

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -35,7 +35,7 @@ export default () => {
         {
           cityCards?.map((city: object) => {
             return (
-              <SwiperSlide><CityCard city={city} /></SwiperSlide>
+              <SwiperSlide><CityCard /></SwiperSlide>
             )
           })
         }
