3a978de800584ac9ccce88f34ee63cbb11fccfda
Renan <renanvgmartins@hotmail.com>
2024-12-27 17:13:24 -0300

    Config de deploy

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 import { CiHeart } from "react-icons/ci";
+import { Link } from "react-router-dom";
 
 
 
@@ -10,8 +11,12 @@ export default function Footer() {
             <div className="max-w-container m-auto flex justify-between px-6 py-4">
                 <h1 className="text-lg flex items-center gap-1">Developed By Renan <CiHeart size={25}/></h1>
                 <div className="flex gap-2">
-                    <FaGithub size={30}/>
-                    <FaLinkedin size={30}/>
+                    <Link to={'https://github.com/Valverd'} target="_blank">
+                        <FaGithub size={30}/>
+                    </Link>
+                    <Link to={'https://www.linkedin.com/in/renan-valverde/'} target="_blank">
+                        <FaLinkedin size={30}/>
+                    </Link>
                 </div>
             </div>
         </footer>
